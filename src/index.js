b34c6005c2cca89a90d9c438b787a0bfa72dea8f
Eknoor Oberoi <38902676+eknooroberoi@users.noreply.github.com>
2020-06-15 12:58:03 +0530

    added current year
    javascript getFullYear()

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 const fname = "Eknoor";
 const lname = "Oberoi";
 const no = "1";
+//const year="2020";
 ReactDOM.render(
   <div>
     <h1>Hello {fname + " " + lname}</h1>
@@ -12,10 +12,11 @@ ReactDOM.render(
     </h1>
 
     <p>My lucky number is {Math.floor(Math.random() * 10)}</p>
+    <p>Created by {fname + " " + lname}</p>
+    <p>Copyright {new Date().getFullYear()}</p>
   </div>,
   document.getElementById("root")
 );
-
 //by using JSX, we were able to insert HTML into JavaScript.
 //It actually lets us add HTML inside a JavaScript
 //file and then insert JavaScript inside that HTML
@@ -23,7 +24,8 @@ ReactDOM.render(
 //JSX is javascript XML, helps to write HTML inside js
 //big difference between expressions and statements is that an expression will be evaluated to
 //a value right? Like it ends up, after all the code's been executed,
-//it ends up equaling something. But this(statement) instead is actually asking the computer to do some work to evaluate
+//it ends up equaling something. But this(statement) instead is actually asking the computer
+//to do some work to evaluate
 //this statement and then depending on that statement work out something.
 //statements control and perform actions but do not become a value
 //expression always resolve to a value
