8b0aea507c75ec913eb1fb85e293afd68a7a36a9
Eknoor Oberoi <38902676+eknooroberoi@users.noreply.github.com>
2020-06-16 01:50:09 +0530

    calculator app using REACT

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import pi, { doublePi, triplePi } from "./math.js";
+import { add, multiply, subtract, divide } from "./calculator.js";
+//or
+//import * as Calculator from "./calculator.js";   // importing everything
+//Import the add, multiply, subtract and divide functions
+//from the calculator.js file.
+//If successful, your website should look the same as the Final.png
+
+////////////////////////////or//////////////////////
+// ReactDOM.render(
+//   <ul>
+//     <li>{Calculator.add(1, 2)}</li>
+//     <li>{Calculator.multiply(2, 3)}</li>
+//     <li>{Calculator.subtract(7, 2)}</li>
+//     <li>{Calculator.divide(5, 2)}</li>
+//   </ul>,
+//   document.getElementById("root")
+// );
 
 ReactDOM.render(
   <ul>
-    <li>{pi}</li>
-    <li>{doublePi()}</li>
-    <li>{triplePi()}</li>
+    <li>{add(1, 2)}</li>
+    <li>{multiply(2, 3)}</li>
+    <li>{subtract(7, 2)}</li>
+    <li>{divide(5, 2)}</li>
   </ul>,
   document.getElementById("root")
 );
