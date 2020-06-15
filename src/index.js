b1250863c7fd540f70272637aabdc29c22df7e46
Eknoor Oberoi <38902676+eknooroberoi@users.noreply.github.com>
2020-06-16 01:36:36 +0530

    Javascript ES6- import, export and modules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-//So components really allow us to split up a large file or a complex web structure into smaller components.
-//And we also get the added benefit of reusing each of these components when we need the same functionality
-//which happens a lot on web.
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
-//we can use custom heading component inside the react code as if it was html element
-ReactDOM.render(<App />, document.getElementById("root"));
+import pi, { doublePi, triplePi } from "./math.js";
+
+ReactDOM.render(
+  <ul>
+    <li>{pi}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
+  </ul>,
+  document.getElementById("root")
+);
