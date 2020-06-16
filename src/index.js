06498625d8b294f9aa257619e1171516bb5eefa1
Eknoor Oberoi <38902676+eknooroberoi@users.noreply.github.com>
2020-06-16 18:22:44 +0530

    REACT props

--- src/index.js
+++ src/index.js
@@ -1,29 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { add, multiply, subtract, divide } from "./calculator.js";
-//or
-//import * as Calculator from "./calculator.js";   // importing everything
-//Import the add, multiply, subtract and divide functions
-//from the calculator.js file.
-//If successful, your website should look the same as the Final.png
 
-////////////////////////////or//////////////////////
-// ReactDOM.render(
-//   <ul>
-//     <li>{Calculator.add(1, 2)}</li>
-//     <li>{Calculator.multiply(2, 3)}</li>
-//     <li>{Calculator.subtract(7, 2)}</li>
-//     <li>{Calculator.divide(5, 2)}</li>
-//   </ul>,
-//   document.getElementById("root")
-// );
+//react component properties are called props
+function Card(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar_img" />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <ul>
-    <li>{add(1, 2)}</li>
-    <li>{multiply(2, 3)}</li>
-    <li>{subtract(7, 2)}</li>
-    <li>{divide(5, 2)}</li>
-  </ul>,
+  <div>
+    <h1>My Contacts</h1>
+    <Card
+      name="Beyonce"
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      tel="+123 456 789"
+      email="b@beyonce.com"
+    />
+    <Card
+      name="Jack Bauer"
+      img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+      tel="+7387384587"
+      email="jack@nowhere.com"
+    />
+  </div>,
   document.getElementById("root")
 );
