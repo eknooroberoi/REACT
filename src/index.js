f60a7ea9814d762cbc04c2295f9226a05ddde50b
Eknoor Oberoi <38902676+eknooroberoi@users.noreply.github.com>
2020-06-16 18:36:58 +0530

    React procs practice

--- src/index.js
+++ src/index.js
@@ -1,33 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-//react component properties are called props
-function Card(props) {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <img src={props.img} alt="avatar_img" />
-      <p>{props.tel}</p>
-      <p>{props.email}</p>
-    </div>
-  );
-}
+ReactDOM.render(<App />, document.getElementById("root"));
 
-ReactDOM.render(
-  <div>
-    <h1>My Contacts</h1>
-    <Card
-      name="Beyonce"
-      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      tel="+123 456 789"
-      email="b@beyonce.com"
-    />
-    <Card
-      name="Jack Bauer"
-      img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-      tel="+7387384587"
-      email="jack@nowhere.com"
-    />
-  </div>,
-  document.getElementById("root")
-);
+//1. Apply CSS styles to App.jsx component
+//to match the appearance on the completed app:
+//https://c6fkx.csb.app/
+//2. Extract the contact card as a reusable Card component.
+//3. Use props to render the default Beyonce contact card.
+//4. Use the contacts.js file to create card components.
